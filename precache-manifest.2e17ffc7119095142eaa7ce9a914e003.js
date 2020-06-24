0152a7e2a67e188b414eba657c602d6f8d6f3eff
derian-rods <65286869+derian-rods@users.noreply.github.com>
2020-06-24 22:40:12 +0200

    Add files via upload

---
+++ precache-manifest.2e17ffc7119095142eaa7ce9a914e003.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4be169e6648f43e7201897ac7be71038",
+    "url": "/index.html"
+  },
+  {
+    "revision": "307f80c796adde5fa932",
+    "url": "/static/css/main.c83ccad9.chunk.css"
+  },
+  {
+    "revision": "2726283877a9ceee20b8",
+    "url": "/static/js/2.f556443b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f556443b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "307f80c796adde5fa932",
+    "url": "/static/js/main.a3c960f3.chunk.js"
+  },
+  {
+    "revision": "7981d4addd9d8e4ab559",
+    "url": "/static/js/runtime-main.ee0c036f.js"
+  }
+]);
